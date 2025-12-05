8c1e4b81c7182fecb5ab7a75769ab7c07812d78b
airuma <117159340+airuma@users.noreply.github.com>
2025-12-05 15:57:51 +0500

    Add files via upload

--- create_tables.sql
+++ create_tables.sql
@@ -1,18 +1,16 @@
--- 1. Таблица Пользователей
 CREATE TABLE users (
     user_id BIGINT PRIMARY KEY, -- ID из Telegram
     username VARCHAR(255),
     first_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 2. Таблица Категорий (Связь 1:Many с users)
+
 CREATE TABLE categories (
     category_id SERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(user_id) ON DELETE CASCADE,
     category_name VARCHAR(100) NOT NULL
 );
 
--- 3. Таблица Задач (Связь 1:Many с users и categories)
 CREATE TABLE tasks (
     task_id SERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(user_id) ON DELETE CASCADE,
@@ -22,8 +20,7 @@ CREATE TABLE tasks (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 4. Таблица Напоминаний (Связь 1:1 с tasks)
--- Выполняет критерий о наличии разных типов связей
+
 CREATE TABLE reminders (
     reminder_id SERIAL PRIMARY KEY,
     task_id INT UNIQUE REFERENCES tasks(task_id) ON DELETE CASCADE,
@@ -31,11 +28,10 @@ CREATE TABLE reminders (
     is_sent BOOLEAN DEFAULT FALSE
 );
 
--- 5. Таблица Логов (Связь 1:Many с users)
--- Выполняет критерий о безопасности и отслеживании действий
+
 CREATE TABLE logs (
     log_id SERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(user_id),
-    action_type VARCHAR(50), -- например: 'ADD_TASK', 'COMPLETE_TASK'
+    action_type VARCHAR(50), 
     action_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
